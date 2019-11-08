157837e2e977dd6bcac4bdeffb03add431e57904
santicr <santicr@javerianacali.edu.co>
2019-11-08 15:19:49 -0500

    551 solved, 673 solved
    551 solved, 673 solved.

--- UVa/551/551.cpp
+++ UVa/551/551.cpp
@@ -1,27 +1,119 @@
 #include <iostream>
-#include <queue>
-#include <stack>
 #include <string>
+#include <stack>
 
 using namespace std;
 
-bool verifica(char s){
-	if(s == '(' || s == ')' || s == '[' || s == ']' || s == '{' || s == '}' ||
-		s == '<' || s == '>'|| s == '*')
-		return true;
-	return false;
-}
+struct signo{
+	int pos;
+	char bracket;
+};
 
 int main(){
 	string str;
 	while(getline(cin, str)){
-		stack <char> pilita;
-		queue <char> pilita2;
+		int j = 1;
+		stack <signo> pila;
+		bool paso = false;
 		for(int i = 0; i < str.size(); i++){
-			if(verifica(str[i])){
-				
+			if(str[i] == '[' || str[i] == '<' || str[i] == '{' || str[i] == '('){
+				signo temp;
+				temp.bracket = str[i];
+				temp.pos = j;
+				pila.push(temp);
+				j++;
 			}
+			else if(str[i] == ']' || str[i] == '>' || str[i] == '}' || str[i] == ')'){
+				signo temp;
+				temp.pos = j;
+				temp.bracket = str[i];
+				if(temp.bracket == ']'){
+					if(pila.empty()){
+						cout << "NO " << j<< endl;
+						paso = true;
+						break;
+					}
+					else if(pila.top().bracket != '['){
+						cout << "NO " << j << endl;
+						paso = true;
+						break;
+					}
+					pila.pop();
+					j++;
+				}
+				else if(temp.bracket == ')'){
+					if(pila.empty()){
+						cout << "NO " << j << endl;
+						paso = true;
+						break;
+					}
+					else if(pila.top().bracket != '('){
+						cout << "NO " << j << endl;
+						paso = true;
+						break;
+					}
+					pila.pop();
+					j++;
+				}
+				else if(temp.bracket == '}'){
+					if(pila.empty()){
+						cout << "NO " << j << endl;
+						paso = true;
+						break;
+					}
+					else if(pila.top().bracket != '{'){
+						cout << "NO " << j << endl;
+						paso = true;
+						break;
+					}
+					pila.pop();
+					j++;
+				}
+				else if(temp.bracket == '>'){
+					if(pila.empty()){
+						cout << "NO " << j << endl;
+						paso = true;
+						break;
+					}
+					else if(pila.top().bracket != '<'){
+						cout << "NO " << j << endl;
+						paso = true;
+						break;
+					}
+					pila.pop();
+					j++;
+				}
+			}
+			else if(str[i] == '*'){
+				if(str[i - 1] == '('){
+					signo temp;
+					temp.bracket = '*';
+					temp.pos = pila.top().pos;
+					pila.push(temp);
+				}
+				else if(str[i - 1] != '(' && str[i + 1] == ')'){
+					if(pila.empty()){
+						cout << "NO " << j << endl;
+						paso = true;
+						break;
+					}
+					else if(pila.top().bracket != '*'){
+						cout << "NO " << j << endl;
+						paso = true;
+						break;
+					}
+					pila.pop();
+				}
+				else
+					j++;
+			}
+			else
+				j++;
 		}
+		if(pila.empty() && paso == false)
+			cout << "YES" << endl;
+		if(!pila.empty() && paso == false)
+			cout << "NO " << j << endl;
 	}
 
 	return 0;
--- UVa/673/673.cpp
+++ UVa/673/673.cpp
@@ -1,60 +1,44 @@
 #include <iostream>
 #include <stack>
-#include <string>
-#include <queue>
 
 using namespace std;
 
-bool balance(string str, queue <char> cola, stack <char> pila){
+bool func(string str){
+	stack <char> pila;
 	for(int i = 0; i < str.size(); i++){
-		if(str[i] == '[' || str[i] == '(')
+		if(str[i] == '(' || str[i] == '[')
 			pila.push(str[i]);
-		else if(str[i] == ']' || str[i] == ')'){
-			cola.push(str[i]);
+		else if(str[i] == ')'){
 			if(pila.empty())
 				return false;
-			if(cola.front() == ']'){
-				if(pila.top() != '[' || pila.empty() == true)
-					return false;
-				cola.pop();
-				pila.pop();
-			}
-			if(cola.front() == ')'){
-				if(pila.top() != '(' || pila.empty() == true)
-					return false;
-				cola.pop();
-				pila.pop();
-			}
+			if(pila.top() != '(')
+				return false;
+			pila.pop();
+		}
+		else if(str[i] == ']'){
+			if(pila.empty())
+				return false;
+			if(pila.top() != '[')
+				return false;
+			pila.pop();
 		}
 	}
-	if(pila.size() > 0 || cola.size() > 0)
+	if(!pila.empty())
 		return false;
-
 	return true;
 }
 
 int main(){
-	int cases, i = 0;
+	int cases;
 	cin >> cases;
+	cin.ignore();
 	while(cases--){
-		string str, str2;
-		queue <char> cola;
-		stack <char> pila;
-		if(i == 0){
-			i++;
-			cin.ignore();
-		}
+		string str;
 		getline(cin, str);
-		for(int i = 0; i < str.size(); i++){
-			if(str[i] != ' ')
-				str2.push_back(str[i]);
-		}
-		if(balance(str2, cola, pila) || str.size() == 0)
+		if(func(str))
 			cout << "Yes" << endl;
 		else
 			cout << "No" << endl;
-
 	}
-
 	return 0;
 }
