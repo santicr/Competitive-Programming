edbfe021b2ce0f31f5ffb2835eca0c04cb17a4e6
santicr <santicr@javerianacali.edu.co>
2019-11-14 17:18:39 -0500

    727 solved, starting 11492

--- UVa/11492/11492.cpp
+++ UVa/11492/11492.cpp
@@ -1,31 +1,11 @@
 #include <iostream>
+#include <stack>
 #include <string>
-#include <vector>
 
-struct trio{
-	string s1, s2, s3;
-};
+using namespace std;
 
 int main(){
-	int cases;
-	string str1, str2, str3, str4, str5;
-	while(cin >> cases){
-		vector <trio> vec;
-		cin >> str1 >> str2;
-		for(int i = 0; i < cases; i++){
-			cin >> str3 >> str4 >> str5;
-			trio temp;
-			temp.s1 = str3;
-			temp.s2 = str4;
-			temp.s3 = str5;
-			vec.push_back(temp);
-		}
-		for(int i = 0; i < vec.size(); i++){
-			if(vec[i].s1 == str1){
-				
-			}
-		}
-	}
+	
 
 	return 0;
 }
--- UVa/727/727.cpp
+++ UVa/727/727.cpp
@@ -23,7 +23,39 @@ int main(){
 				break;
 			str.push_back(str1[0]);
 		}
-		cout << str << endl;
+		for(int i = 0; i < str.size(); i++){
+			if(str[i] == '*' || str[i] == '+' || str[i] == '-' || str[i] == '/'){
+				if(pila.empty() || valor(str[i]) > valor(pila.top()))
+					pila.push(str[i]);
+				else{
+					while(!(valor(pila.top()) < valor(str[i]))){
+						cout << pila.top();
+						pila.pop();
+						if(pila.empty())
+							break;
+					}
+					pila.push(str[i]);
+				}
+			}
+			else if(str[i] == '(')
+				pila.push(str[i]);
+			else if(str[i] == ')'){
+				while(pila.top() != '('){
+					cout << pila.top();
+					pila.pop();
+				}
+				pila.pop();
+			}
+			else
+				cout << str[i];
+		}
+		while(!pila.empty()){
+			cout << pila.top();
+			pila.pop();
+		}
+		cout << endl;
+		if(cases != 0)
+			cout << endl;
 	}
 
 
