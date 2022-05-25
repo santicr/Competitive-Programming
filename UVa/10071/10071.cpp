dcaef9e6b0125d4256aaad0f3b4fc3d582f485c9
santicr <santicr@javerianacali.edu.co>
2022-05-25 16:57:16 -0500

    10071 Solved.
    10071 Solved. Just distance formula.

---
+++ UVa/10071/10071.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+//Author: Santiago Caicedo Rojas
+//Github: https://github.com/santicr
+//UVa profile: https://uhunt.onlinejudge.org/id/1033486
+
+using namespace std;
+
+int main(){
+	int v, t;
+	while(cin >> v){
+		cin >> t;
+
+		cout << (2 * t) * v << endl;
+	}
+
+	return 0;
+}
---
+++ UVa/template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+//Author: Santiago Caicedo Rojas
+//Github: https://github.com/santicr
+//UVa profile: https://uhunt.onlinejudge.org/id/1033486
+
+using namespace std;
+
+int main(){
+
+	return 0;
+}
