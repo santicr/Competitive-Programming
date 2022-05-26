0bf0607b89a479dcef45855d4e6f9c4a921024d4
santicr <santicr@javerianacali.edu.co>
2022-05-26 09:09:45 -0500

    Template updated
    Template updated

--- UVa/template.cpp
+++ UVa/template.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
-//Author: Santiago Caicedo Rojas
-//Github: https://github.com/santicr
-//UVa profile: https://uhunt.onlinejudge.org/id/1033486
+/*
+Author: Santiago Caicedo Rojas
+Github: https://github.com/santicr
+UVa profile: https://uhunt.onlinejudge.org/id/1033486
+*/
 
 using namespace std;
 
